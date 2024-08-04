42e331f2ae19a63c7fea69b69b1b7b4f8946f6c7
bnm <bartekbata1302@gmail.com>
2024-08-04 20:43:48 +0200

    disable watching, pulling automagically restarts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
     apps : [{
       name   : "skyblock-calendar",
       script : "./calendar.js",
-      watch : ['calendar.js'],
-      watch_delay : 1000,
       log_file : "./calendar.log",
     }]
 }
